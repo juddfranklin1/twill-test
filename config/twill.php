7b05a113bcdb90017952d46b10afb5168137f499
Judd Franklin <judd@fountainheadme.com>
2019-07-26 07:42:52 -0700

    defaulting media library to local

--- config/twill.php
+++ config/twill.php
@@ -1,4 +1,9 @@
 <?php
 
 return [
+    'media_library' => [
+        'endpoint_type' => env('MEDIA_LIBRARY_ENDPOINT_TYPE', 'local'),
+        'cascade_delete' => env('MEDIA_LIBRARY_CASCADE_DELETE', false),
+        'local_path' => env('MEDIA_LIBRARY_LOCAL_PATH', 'uploads/'),
+    ],
 ];
