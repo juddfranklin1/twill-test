868bd588e6882ffaea40f1c2b2a1de6e8dd43d5c
Judd Franklin <judd@fountainheadme.com>
2019-07-26 08:20:32 -0700

    default media library to forgo imgix and go local

--- config/twill.php
+++ config/twill.php
@@ -5,5 +5,6 @@ return [
         'endpoint_type' => env('MEDIA_LIBRARY_ENDPOINT_TYPE', 'local'),
         'cascade_delete' => env('MEDIA_LIBRARY_CASCADE_DELETE', false),
         'local_path' => env('MEDIA_LIBRARY_LOCAL_PATH', 'uploads/'),
+        'image_service' => env('MEDIA_LIBRARY_IMAGE_SERVICE', 'A17\Twill\Services\MediaLibrary\Local'),
     ],
 ];
