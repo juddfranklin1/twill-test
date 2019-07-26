fe3bb0aae3823a8c55693673fb8cb9596c34dff7
Judd Franklin <judd@fountainheadme.com>
2019-07-26 08:24:10 -0700

    php artisan twill:module caseStudies -BSMFPR

---
+++ app/Models/Revisions/CaseStudyRevision.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Revisions;
+
+use A17\Twill\Models\Revision;
+
+class CaseStudyRevision extends Revision
+{
+    protected $table = "case_study_revisions";
+}
---
+++ app/Models/Slugs/CaseStudySlug.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Slugs;
+
+use A17\Twill\Models\Model;
+
+class CaseStudySlug extends Model
+{
+    protected $table = "case_study_slugs";
+}
